5dc422e88e00e43ef9d3e365d7c16a95981effeb
Wiktor Walczak <w.walczak93@gmail.com>
2020-01-04 16:28:17 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import CalcPuzzle from "./CalcPuzzle";
+
+function App() {
+  return (
+    <>
+      <h1>Sport calculators</h1>
+      <CalcPuzzle></CalcPuzzle>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/CalcPuzzle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CalcPuzzle = () => {
+  return <h2>cardpuzzle</h2>;
+};
+
+export default CalcPuzzle;
