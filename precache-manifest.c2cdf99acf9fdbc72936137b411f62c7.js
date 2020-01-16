9151eefde1bfa01d20d81998c2a950598821ca19
Wiktor Walczak <w.walczak93@gmail.com>
2020-01-17 00:02:59 +0100

    Updates

---
+++ precache-manifest.c2cdf99acf9fdbc72936137b411f62c7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3802d7a5724ad0d9613aea10dcc3902c",
+    "url": "/sport-calculators/index.html"
+  },
+  {
+    "revision": "0a580bc88dde1a476768",
+    "url": "/sport-calculators/static/css/main.262a1f4e.chunk.css"
+  },
+  {
+    "revision": "e0ed1dfb4f356a8277e8",
+    "url": "/sport-calculators/static/js/2.99787338.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/sport-calculators/static/js/2.99787338.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a580bc88dde1a476768",
+    "url": "/sport-calculators/static/js/main.ca34074c.chunk.js"
+  },
+  {
+    "revision": "9120ed9d043175686269",
+    "url": "/sport-calculators/static/js/runtime-main.a030a601.js"
+  },
+  {
+    "revision": "0c155670c95dc361f273f3f89acc165a",
+    "url": "/sport-calculators/static/media/cycling.0c155670.svg"
+  },
+  {
+    "revision": "4bf72df0c45bb41ede393ecddbd474d7",
+    "url": "/sport-calculators/static/media/gym.4bf72df0.svg"
+  },
+  {
+    "revision": "efc2fd96ac21f517784f0bdb0c13ac83",
+    "url": "/sport-calculators/static/media/heart-rate.efc2fd96.svg"
+  },
+  {
+    "revision": "ad17a9092c27340e6a2af3e850ee121d",
+    "url": "/sport-calculators/static/media/running.ad17a909.svg"
+  },
+  {
+    "revision": "e7c181a4c399dceaf6d8c3ef64ce15dc",
+    "url": "/sport-calculators/static/media/swimming.e7c181a4.svg"
+  }
+]);
